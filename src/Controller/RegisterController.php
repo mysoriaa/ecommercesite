4f8ba8105362772a13111866bae9be8911aa33f3
mysoriaa <maissaabdelli.ma@gmail.com>
2025-02-03 15:03:26 +0100

    ajout page connexion et création AccountController

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,24 +9,32 @@ use App\Form\RegisterUserType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\User;
-final class RegisterController extends AbstractController
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface; // Import pour le hachage du mot de passe
 
+final class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {   
         $user = new User();
-        $form = $this->createForm(RegisterUserType::class,$user);
+        $form = $this->createForm(RegisterUserType::class, $user);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            // $user = $form->getData();
-            // $entityManager = $this->getDoctrine()->getManager();
+            // Récupérer le mot de passe brut depuis le formulaire
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            // Hacher le mot de passe
+            $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword); // Assigner le mot de passe haché à l'entité User
+
+            // Sauvegarder l'utilisateur
             $entityManager->persist($user);
             $entityManager->flush();
-            // return $this->redirectToRoute('app_login');
+
+            return $this->redirectToRoute('app_login'); // Redirection après inscription réussie
         }
+
         return $this->render('register/index.html.twig', [
             'registerForm' => $form->createView(),
         ]);
